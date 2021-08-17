c5bfa2ab510d802b5221c30f546c5a511f75b98f
Dirceu Jr <dirceuu@gmail.com>
2021-08-17 14:48:08 -0300

    Update tasks.rb

--- lib/mina/slack/tasks.rb
+++ lib/mina/slack/tasks.rb
@@ -7,9 +7,9 @@ require 'mina'
 # Slack tasks
 namespace :slack do
   # Required
-  set :slack_token,       -> { ENV['SLACK_TOKEN'] }
-  set :slack_room,        -> { ENV['SLACK_ROOM'] }
-  set :slack_subdomain,   -> { ENV['SLACK_SUBDOMAIN'] }
+  set :slack_token,       -> { ENV['SLACK_TOKEN'] || fetch(:slack_token) }
+  set :slack_room,        -> { ENV['SLACK_ROOM'] || fetch(:slack_room) }
+  set :slack_subdomain,   -> { ENV['SLACK_SUBDOMAIN'] || fetch(:slack_subdomain) }
   # Optional
   set :slack_stage,       -> { ENV['SLACK_STAGE'] || fetch(:rails_env, 'production') }
   set :slack_application, -> { ENV['SLACK_APPLICATION'] || fetch(:application_name) }
